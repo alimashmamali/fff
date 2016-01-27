21084d269e2b157cd3d4fc674ad8ea292fe469b6
shatelteam <Shateloriginal@gmail.com>
2016-01-27 02:25:18 -0800

    Create Version.lua

---
+++ plugins/Version.lua
@@ -0,0 +1,40 @@
+do
+
+function run(msg, matches)
+  return 'SHATEL TEAM'.. [[ 
+  
+ 😊sha🚀tel bot🤖💦
+💎V2🔪
+🏆POWERED BYE💪🏻
+☠AMIR BLACK WOLF☠
+🙏Special Thanks to @ThisIsamirh for anything✊
+💀sudo😈
+@Amirm_wolfshon
+😃admins😴
+@Hazrateh_Bi_Asab
+@skywhite
+@nothing_am
+@Lompan
+@ananymous0
+@mr_professor
+@sokot1010
+@X_tfb_sudo
+👻our team bot🤖
+@teleover
+@telehard
+🔥ZS🔥 📛group📛
+Made In🇮🇷
+Our Channel🗣
+@shatel_team]]
+end
+
+return {
+  description = "Robot and Creator About", 
+  usage = "/ver : robot info",
+  patterns = {
+    "^[!/]ver$"
+  }, 
+  run = run 
+}
+
+end
