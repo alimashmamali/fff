e722708bb6f84757ff7ae633bf907e4c8c40bbd4
alimashmamali <alimashmamali@30mail.ir>
2016-02-01 22:03:43 +0330

    Update Version.lua

--- plugins/Version.lua
+++ plugins/Version.lua
@@ -1,38 +1,16 @@
 do
 
 function run(msg, matches)
-  return 'SHATEL TEAM'.. [[ 
-  
- 😊sha🚀tel bot🤖💦
-💎V2🔪
-🏆POWERED BYE💪🏻
-☠AMIR BLACK WOLF☠
-🙏Special Thanks to @ThisIsamirh for anything✊
-💀sudo😈
-@Amirm_wolfshon
-😃admins😴
-@Hazrateh_Bi_Asab
-@skywhite
-@nothing_am
-@Lompan
-@ananymous0
-@mr_professor
-@sokot1010
-@X_tfb_sudo
-👻our team bot🤖
-@teleover
-@telehard
-🔥ZS🔥 📛group📛
-Made In🇮🇷
-Our Channel🗣
-@shatel_team]]
+  return 'TeLeMaGmA BoT'.. [[ 
+  beta 1
+  amir w.h]]
 end
 
 return {
   description = "Robot and Creator About", 
   usage = "/ver : robot info",
   patterns = {
-    "^[!/]ver$"
+    "^ver$"
   }, 
   run = run 
 }
